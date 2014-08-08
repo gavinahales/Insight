13eac3027d861e82c659f747dac8e7eb0d661e41
Gavin Hales <gavinahales@gmail.com>
2014-08-08 15:59:29 +0100

    Added event type code to DetailWindow. Identified better method signature for DetailWindow constructor, added as comment for the moment.

--- Insight/DetailWindow.xaml.cs
+++ Insight/DetailWindow.xaml.cs
@@ -24,12 +24,37 @@ namespace Insight
             InitializeComponent();
         }
 
+        //Bit of a stupid way to parameterise this constructor. Why not just pass over entire event object and then strip out the rquired info in this method? Would ease any extensions.
         public DetailWindow(String id, String link, String accessed)
         {
             InitializeComponent();
+
+            String eventType = null;
+
+            switch (id.ToLower().Substring(0,5))
+            {
+                case "autwh":
+                    eventType = "Web History (Autopsy)";
+                    break;
+                case "autad":
+                    eventType = "Attached Device (Autopsy)";
+                    break;
+                case "autip":
+                    eventType = "Installed Program (Autopsy)";
+                    break;
+                case "autex":
+                    eventType = "EXIF Metadata (Autopsy)";
+                    break;
+                default:
+                    eventType = "Unknown";
+                    break;
+                    
+
+            }
+
             lblAccessed.Content = accessed;
             lblLink.Content = link;
-            lblEventType.Content = "Unknown";
+            lblEventType.Content = eventType;
         }
     }
 }
