c1b3522713b77bc5de38d0d07c03425a036d8db6
Gavin Hales <gavinahales@gmail.com>
2014-11-13 02:20:39 +0000

    Completed timer app

--- InsightTimer/MainWindow.xaml.cs
+++ InsightTimer/MainWindow.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using GuerrillaNtp;
-using MySql.Data.MySqlClient;
+using System.Net.Http;
+using RestSharp;
 
 namespace InsightTimer
 {
@@ -22,26 +22,20 @@ namespace InsightTimer
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const string NTP_SERVER = "0.uk.pool.ntp.org";
-        public const string MYSQL_SERVER = "178.62.25.124";
+        public const string INSIGHT_SERVER = "http://insight.mogwai-tech.xyz/";
 
         public MainWindow()
         {
             InitializeComponent();
 
-            //this.Closing += MainWindow_Closing;
+            this.Closing += MainWindow_Closing;
             this.Loaded += MainWindow_Loaded;
             this.ContentRendered += MainWindow_ContentRendered;
         }
 
         private void MainWindow_ContentRendered(object sender, EventArgs e)
         {
-            bool connected = TestConnectivity();
-
-            if (connected)
-            {
-                lblStatus.Text = "Please enter your Participant ID, and start the experiment when ready.";
-            }
+            TestConnectivity();
         }
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -59,35 +53,98 @@ namespace InsightTimer
         {
             try
             {
-                GuerrillaNtp.NtpClient ntpclient = new NtpClient(NTP_SERVER);
-                ntpclient.Timeout = TimeSpan.FromSeconds(4);
-                NtpPacket response = ntpclient.Query();
-                DateTime time = (DateTime)response.ReceiveTimestamp;
-                //MessageBox.Show(time.ToLongDateString() + " " + time.ToLongTimeString());
+                RestClient client = new RestClient(INSIGHT_SERVER);
+                client.Timeout=5000;
+                RestRequest request = new RestRequest("testconnection.html", Method.GET);
+                RestResponse response = (RestResponse)client.Execute(request);
+
+                if (response.ResponseStatus == ResponseStatus.Completed && response.Content=="OK")
+                {
+                    lblStatus.Text = "Please enter your participant ID:";
+                    return true;
+                }
+                else if (response.ResponseStatus == ResponseStatus.Completed && response.Content != "OK")
+                {
+                    lblStatus.Text = "Connectivity tests failed. Unexpected response: \n" + response.Content;
+                    btnStart.IsEnabled = false;
+                    return false;
+                }
+                else
+                {
+                    lblStatus.Text = "Connectivity tests failed. Error: " + response.ErrorMessage;
+                    btnStart.IsEnabled = false;
+                    return false;
+                }
+
             }
+            
             catch (Exception e)
             {
-                lblStatus.Text = "Connection tests FAILED. NTP Error:\n" + e.Message;
-                MessageBox.Show("Connection tests have failed. Please alert Gavin.", "Connection Failure", MessageBoxButton.OK, MessageBoxImage.Error);
-                //return false;
-            }
-
-            try
-            {
-                MySqlConnection mysqlconn = new MySqlConnection("Server=" + MYSQL_SERVER + ";Database=test;Uid=resulter;PASSWORD=insightexternal4501;");
-                mysqlconn.Open();
-
-            }
-            catch (Exception e)
-            {
-                lblStatus.Text = "Connection tests FAILED. MySQL Error:\n" + e.Message;
-                MessageBox.Show("Connection tests have failed. Please alert Gavin.", "Connection Failure", MessageBoxButton.OK, MessageBoxImage.Error);
+                lblStatus.Text = "Connectivity tests failed. Error: " + e.Message;
                 return false;
             }
 
-            //Test connection to MySQL database here
+        }
 
-            return true;
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtParticipantID.Text.Trim() == "")
+            {
+                MessageBox.Show("You need to enter your participant ID!", "ID Needed", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                RestClient client = new RestClient(INSIGHT_SERVER);
+                client.Timeout = 5000;
+                RestRequest request = new RestRequest("resultSubmit.php", Method.POST);
+                request.AddParameter("APIKey", "INSIGHT_APIKEY_4501");
+                request.AddParameter("partID", txtParticipantID.Text);
+                request.AddParameter("timeType", "S");
+                RestResponse response = (RestResponse)client.Execute(request);
+
+                if (response.ResponseStatus == ResponseStatus.Completed && response.Content == "SUCCESS")
+                {
+                    btnStart.IsEnabled = false;
+                    btnStop.IsEnabled = true;
+                    txtParticipantID.IsEnabled = false;
+                    lblStatus.Text = "Experiment Running...";
+                }
+                else
+                {
+                    lblStatus.Text = "Server failure. Experiment cannot be started!";
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtParticipantID.Text.Trim() == "")
+            {
+                MessageBox.Show("You need to enter your participant ID!", "ID Needed", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                RestClient client = new RestClient(INSIGHT_SERVER);
+                client.Timeout = 5000;
+                RestRequest request = new RestRequest("resultSubmit.php", Method.POST);
+                request.AddParameter("APIKey", "INSIGHT_APIKEY_4501");
+                request.AddParameter("partID", txtParticipantID.Text);
+                request.AddParameter("timeType", "F");
+                RestResponse response = (RestResponse)client.Execute(request);
+
+                if (response.ResponseStatus == ResponseStatus.Completed && response.Content == "SUCCESS")
+                {
+                    btnStop.IsEnabled = false;
+                    lblStatus.Text = "Experiment Complete! Thank you!";
+                    this.Closing -= MainWindow_Closing;
+                }
+                else
+                {
+                    lblStatus.Text = "Server failure. Experiment cannot be stopped!";
+                    MessageBox.Show("Contact with server lost! Experiment cannot be recorded as stopped, please alert Gavin!", "Server Failure!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
         }
     }
 }
